9be3967facdab752441dab854f358502c63f6c96
mdhs <mdhs.v1@gmail.com>
2023-04-07 17:49:44 +0800

    fix config

--- conf.lua
+++ conf.lua
@@ -40,11 +40,11 @@ require('lualine').setup({
 require('pears').setup()
 
 -- cmp
+local cmp = require('cmp')
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-local cmp = require('cmp')
 cmp.setup({
   preselect = cmp.PreselectMode.None,
   sources = cmp.config.sources({
@@ -57,15 +57,12 @@ cmp.setup({
   -- behave like suptertab
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif has_words_before() then
+      if has_words_before() then
         cmp.complete()
       else
         fallback()
       end
     end, { "i", "s" }),
-    ['<C-i>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
 })
